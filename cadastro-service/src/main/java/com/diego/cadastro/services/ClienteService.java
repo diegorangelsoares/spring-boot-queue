3ad771c55322594461d59829f3de491d89f4b450
Diego Rangel <diegorangeljpa@gmail.com>
2023-01-05 10:48:45 -0300

    Ajuste de redis

--- cadastro-service/src/main/java/com/diego/cadastro/services/ClienteService.java
+++ cadastro-service/src/main/java/com/diego/cadastro/services/ClienteService.java
@@ -1,6 +1,8 @@
 package com.diego.cadastro.services;
 
 import com.diego.cadastro.model.Cliente;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -9,13 +11,16 @@ import java.util.List;
 
 public interface ClienteService {
 
-    @Cacheable(value = "clientes", key = "#idCliente")
+    @Cacheable(value = "clientes")
     public Cliente getClienteById(Long idCliente);
 
+    @CacheEvict(value = "clientes", allEntries=true)
     public Cliente save (Cliente cliente);
 
+    @Cacheable(value = "clientes")
     public List<Cliente> getAll ();
 
+    @Cacheable(value = "clientes")
     public Page<Cliente> getAllPage(int page, int size);
 
 }
